41f13e716e5629a30d545c336f8860e19450586c
Kayo033 <110784831+Kayo033@users.noreply.github.com>
2023-02-18 19:11:35 -0300

    18/02/23

--- js/main.js
+++
@@ -1,3 +0,0 @@
-function myFunction() {
-    document.getElementById("onclick").innerHTML = "Para quem deseja assistir uma serie em familia, Chuck é uma otima opção. Independente de ser uma serie antiga (2007), continua sendo uma otima opção pois envolve um pouco de tudo desde drama, suspense, ação e aventura."
-}
